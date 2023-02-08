bc0e41abe9b6e93eb39fe01f080a7fe8c3639ce3
Felipe <felipe.arosa@gmail.com>
2023-02-08 19:10:53 -0300

    completed first challenge

---
+++ section-1/challenge-1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+  public static void main(String[] args) {
+    // Instructions for this workbook are on Learn the Part (See the Udemy Video:
+    // Your Initials to access the link).
+    System.out.println("FFFFF    A     SSS ");
+    System.out.println("F       A A   S   S");
+    System.out.println("F      A   A  S    ");
+    System.out.println("FFF    AAAAA   SSS ");
+    System.out.println("F      A   A      S");
+    System.out.println("F      A   A  S   S");
+    System.out.println("F      A   A   SSS ");
+  }
+}
