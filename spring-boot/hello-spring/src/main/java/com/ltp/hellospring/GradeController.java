0c3b836fd4205d881001304784590731a2ecd5b4
Felipe <felipe.arosa@gmail.com>
2023-02-18 23:28:16 -0300

    added updated form functionality

--- spring-boot/hello-spring/src/main/java/com/ltp/hellospring/GradeController.java
+++ spring-boot/hello-spring/src/main/java/com/ltp/hellospring/GradeController.java
@@ -7,14 +7,13 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeController {
 
-  List<Grade> studentsGrades = Arrays.asList(
-      new Grade("Harry", "Potions", "B"),
-      new Grade("Hermine", "Arithmancy", "A"),
-      new Grade("Ron", "Charms", "B-"));
+  List<Grade> studentsGrades = new ArrayList<>();
 
   @GetMapping("/grades")
   public String getGrades(Model model) {
@@ -23,8 +22,36 @@ public class GradeController {
   }
 
   @GetMapping("/")
-  public String gradeForm(Model model) {
-    model.addAttribute("grade", new Grade());
+  public String gradeForm(Model model, @RequestParam(required = false) String name) {
+    Grade grade;
+    if (getGradeIndex(name) == -1000) {
+      grade = new Grade();
+    } else {
+      grade = studentsGrades.get(getGradeIndex(name));
+    }
+
+    model.addAttribute("grade", grade);
     return "form";
   }
+
+  @PostMapping("/new-grade")
+  public String addGrade(Grade grade) {
+    int index = getGradeIndex(grade.getName());
+    if (index == -1000) {
+      studentsGrades.add(grade);
+    } else {
+      studentsGrades.set(index, grade);
+    }
+
+    return "redirect:/grades";
+  }
+
+  public Integer getGradeIndex(String name) {
+    for (int i = 0; i < studentsGrades.size(); i++) {
+      if (studentsGrades.get(i).getName().equals(name))
+        return i;
+    }
+    return -1000;
+  }
+
 }
