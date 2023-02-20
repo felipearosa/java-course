d1c51370152b28db017ff21d842b5d1e9633fe22
Felipe <felipe.arosa@gmail.com>
2023-02-20 00:17:28 -0300

    created model, controller and view with form and inventory

---
+++ spring-boot/challenge-1/global-superstore/src/main/java/com/globalsuperstore/globalsuperstore/Constants.java
@@ -0,0 +1,9 @@
+package com.globalsuperstore.globalsuperstore;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class Constants {
+  public static final List<String> CATEGORIES = Arrays.asList("Furniture", "Office Suplies", "Technology" );
+}
---
+++ spring-boot/challenge-1/global-superstore/src/test/java/com/globalsuperstore/globalsuperstore/GlobalSuperstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalsuperstore.globalsuperstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalSuperstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
