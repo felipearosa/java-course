42ddc276460a8f3a2d1f94eec13e6f778518231a
Felipe <felipe.arosa@gmail.com>
2023-02-21 19:49:30 -0300

    added handler to bad requests with ControllerAdvice

---
+++ spring-boot/rest-api/contacts/src/main/java/com/ltp/contacts/exception/ErrorResponse.java
@@ -0,0 +1,33 @@
+package com.ltp.contacts.exception;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class ErrorResponse {
+  private String message;
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+  private LocalDateTime timestamp;
+
+  public ErrorResponse(String message) {
+    this.message = message;
+    this.timestamp = LocalDateTime.now();
+  }
+
+  public String getMessage() {
+    return this.message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  public LocalDateTime getTimestamp() {
+    return this.timestamp;
+  }
+
+  public void setTimestamp(LocalDateTime timestamp) {
+    this.timestamp = timestamp;
+  }
+
+}
--- spring-boot/rest-api/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
+++ spring-boot/rest-api/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -13,57 +13,43 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ltp.contacts.exception.NoContactException;
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
 
 @RestController
 public class ContactController {
 
-    @Autowired
-    private ContactService contactService;
+  @Autowired
+  private ContactService contactService;
 
-    @GetMapping("/contact/all")
-    public ResponseEntity<List<Contact>> getContacts() {
-        List<Contact> contacts = contactService.getContacts();
-        return new ResponseEntity<>(contacts, HttpStatus.OK);
-    }
+  @GetMapping("/contact/all")
+  public ResponseEntity<List<Contact>> getContacts() {
+    List<Contact> contacts = contactService.getContacts();
+    return new ResponseEntity<>(contacts, HttpStatus.OK);
+  }
 
-    @GetMapping("/contact/{id}")
-    public ResponseEntity<Contact> getContact(@PathVariable String id) {
-      try {
-        Contact contact = contactService.getContactById(id);
-        return new ResponseEntity<>(contact, HttpStatus.OK);
-      } catch (NoContactException e) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-      }
-    }
+  @GetMapping("/contact/{id}")
+  public ResponseEntity<Contact> getContact(@PathVariable String id) {
+    Contact contact = contactService.getContactById(id);
+    return new ResponseEntity<>(contact, HttpStatus.OK);
+  }
 
-    @PostMapping("/contact")
-    public ResponseEntity<HttpStatus> createContact(@RequestBody Contact contact) {
-        contactService.saveContact(contact);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
+  @PostMapping("/contact")
+  public ResponseEntity<HttpStatus> createContact(@RequestBody Contact contact) {
+    contactService.saveContact(contact);
+    return new ResponseEntity<>(HttpStatus.CREATED);
+  }
 
-    @PutMapping("/contact/{id}")
-    public ResponseEntity<Contact> updateContact(@PathVariable String id, @RequestBody Contact contact) {
-      try {
-        contactService.updateContact(id, contact);
-        return new ResponseEntity<Contact>(contactService.getContactById(id), HttpStatus.OK);
-      } catch (NoContactException e) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-      }
-    }
+  @PutMapping("/contact/{id}")
+  public ResponseEntity<Contact> updateContact(@PathVariable String id, @RequestBody Contact contact) {
+    contactService.updateContact(id, contact);
+    return new ResponseEntity<Contact>(contactService.getContactById(id), HttpStatus.OK);
+  }
 
-    @DeleteMapping("/contact/{id}")
-    public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id) {
-      try {
-        contactService.deleteContact(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-      } catch (Exception e) {
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-      }
-    }
-
+  @DeleteMapping("/contact/{id}")
+  public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id) {
+    contactService.deleteContact(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
 
 }
